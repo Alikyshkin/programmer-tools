d64b87e3050fb0db8e4993d25633f8cc054bdd93
Alika Kuznetsova <a.kuznetsova@bsmg.tech>
2024-10-08 10:51:17 +0300

    feat: add images

--- pages/index.vue
+++ pages/index.vue
@@ -12,10 +12,10 @@ import Header from '~/components/AppHeader.vue';
 import Tile from '~/components/AppTile.vue';
 
 const tiles = [
-  { title: "HTML Beautify", description: "Beautify HTML code", image: "/images/html.png", link: "/html-beautify" },
-  { title: "CSS Minify", description: "Minify CSS code", image: "/images/css.png", link: "/css-minify" },
-  { title: "JavaScript Minify", description: "Minify JavaScript code", image: "/images/js.png", link: "/js-minify" },
-  { title: "JavaScript Obfuscator", description: "Obfuscate JavaScript code", image: "/images/obfuscate.png", link: "/js-obfuscate" },
-  { title: "CSS Beautify", description: "Beautify CSS code", image: "/images/css-beautify.png", link: "/css-beautify" }
+  { title: "HTML Beautify", description: "Beautify HTML code", image: "/images/luca-bravo-XJXWbfSo2f0-unsplash.jpg", link: "/html-beautify" },
+  { title: "CSS Minify", description: "Minify CSS code", image: "/images/luca-bravo-XJXWbfSo2f0-unsplash.jpg", link: "/css-minify" },
+  { title: "JavaScript Minify", description: "Minify JavaScript code", image: "/images/luca-bravo-XJXWbfSo2f0-unsplash.jpg", link: "/js-minify" },
+  { title: "JavaScript Obfuscator", description: "Obfuscate JavaScript code", image: "/images/luca-bravo-XJXWbfSo2f0-unsplash.jpg", link: "/js-obfuscate" },
+  { title: "CSS Beautify", description: "Beautify CSS code", image: "/images/luca-bravo-XJXWbfSo2f0-unsplash.jpg", link: "/css-beautify" }
 ];
 </script>
